9c022c15b0ec0881d883ca15ea440ca8824b5a0d
lwgover <lucasgover@gmail.com>
2025-04-16 21:26:17 -0400

    Added logging for ENV

--- pipelines/flows/tx-leg.py
+++ pipelines/flows/tx-leg.py
@@ -25,7 +25,7 @@ logging.basicConfig(filename=LOG_PATH, level=logging.DEBUG)
 # DATA PIPELINE
 ################################################################################
 
-
+@task(retries=0, retry_delay_seconds=10, log_prints=True, cache_policy=NO_CACHE)
 def bills(raw_bills_df, curr_bills_df=None, duckdb_conn = None):
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Starting to process bills data")
     bills_df = get_bills_data(raw_bills_df)
@@ -36,6 +36,7 @@ def bills(raw_bills_df, curr_bills_df=None, duckdb_conn = None):
     log_bq_load('lgover', OUT_DATASET_NAME, 'bills', ENV, 'drop', duckdb_conn)
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Bills data processing complete")
 
+@task(retries=0, retry_delay_seconds=10, log_prints=True, cache_policy=NO_CACHE)
 def actions(raw_bills_df, curr_actions_df=None, duckdb_conn = None):
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Starting to process actions data")
     actions_df = get_actions_data(raw_bills_df)
@@ -47,6 +48,7 @@ def actions(raw_bills_df, curr_actions_df=None, duckdb_conn = None):
     log_bq_load('lgover', OUT_DATASET_NAME, 'actions', ENV, 'drop', duckdb_conn)
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Actions data processing complete")
 
+@task(retries=0, retry_delay_seconds=10, log_prints=True, cache_policy=NO_CACHE)
 def authors(raw_bills_df, curr_authors_df=None, duckdb_conn = None):
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Starting to process authors data")
     authors_df = get_authors_data(raw_bills_df)
@@ -57,6 +59,7 @@ def authors(raw_bills_df, curr_authors_df=None, duckdb_conn = None):
     log_bq_load('lgover', OUT_DATASET_NAME, 'authors', ENV, 'drop', duckdb_conn)
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Authors data processing complete")
 
+@task(retries=0, retry_delay_seconds=10, log_prints=True, cache_policy=NO_CACHE)
 def bill_stages(raw_bills_df, config, curr_bill_stages_df=None, duckdb_conn = None):
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Starting to process bill stages data")
     bill_stages_df = get_bill_stages(config['sources']['html']['bill_stages'], raw_bills_df)
@@ -67,6 +70,7 @@ def bill_stages(raw_bills_df, config, curr_bill_stages_df=None, duckdb_conn = No
     log_bq_load('lgover', OUT_DATASET_NAME, 'bill_stages', ENV, 'drop', duckdb_conn)
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Bill stages data processing complete")
 
+@task(retries=0, retry_delay_seconds=10, log_prints=True, cache_policy=NO_CACHE)
 def bill_texts(duckdb_conn, ftp_conn):
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Starting to process bill texts data")
     first_seen_at = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
@@ -77,6 +81,7 @@ def bill_texts(duckdb_conn, ftp_conn):
     log_bq_load('lgover', OUT_DATASET_NAME, 'bill_texts', ENV, 'append', duckdb_conn)
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Bill texts data processing complete")
 
+@task(retries=0, retry_delay_seconds=10, log_prints=True, cache_policy=NO_CACHE)
 def committee_status(raw_bills_df, curr_committees_df=None, duckdb_conn = None):
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Starting to process committee status data")
     committees_df = get_committee_status_data(raw_bills_df)
@@ -87,6 +92,7 @@ def committee_status(raw_bills_df, curr_committees_df=None, duckdb_conn = None):
     log_bq_load('lgover', OUT_DATASET_NAME, 'committees', ENV, 'drop', duckdb_conn)
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Committees data processing complete")
 
+@task(retries=0, retry_delay_seconds=10, log_prints=True, cache_policy=NO_CACHE)
 def committee_hearing_videos(config, curr_committee_hearing_videos_df=None, duckdb_conn = None):
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Starting to process committee hearing videos data")
     committee_hearing_videos_df = get_committee_hearing_videos_data(config)
@@ -97,6 +103,7 @@ def committee_hearing_videos(config, curr_committee_hearing_videos_df=None, duck
     log_bq_load('lgover', OUT_DATASET_NAME, 'committee_hearing_videos', ENV, 'drop', duckdb_conn)
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Committee hearing videos data processing complete")
 
+@task(retries=0, retry_delay_seconds=10, log_prints=True, cache_policy=NO_CACHE)
 def committee_meetings(config, curr_committee_meetings_df=None, duckdb_conn = None):
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Starting to process committee meetings data")
     committee_meetings_df = get_committee_meetings_data(config)
@@ -106,6 +113,7 @@ def committee_meetings(config, curr_committee_meetings_df=None, duckdb_conn = No
     log_bq_load('lgover', OUT_DATASET_NAME, 'committee_meetings', ENV, 'drop', duckdb_conn)
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Committee meetings data processing complete")
 
+@task(retries=0, retry_delay_seconds=10, log_prints=True, cache_policy=NO_CACHE)
 def committee_meeting_bills(config, curr_committee_meeting_bills_df=None, duckdb_conn = None):
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Starting to process committee meeting bills data")
     committee_meeting_bills_df = get_committee_meeting_bills_data(config)
@@ -116,6 +124,7 @@ def committee_meeting_bills(config, curr_committee_meeting_bills_df=None, duckdb
     log_bq_load('lgover', OUT_DATASET_NAME, 'committee_meeting_bills', ENV, 'drop', duckdb_conn)
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Committee meeting bills data processing complete")
 
+@task(retries=0, retry_delay_seconds=10, log_prints=True, cache_policy=NO_CACHE)
 def committee_meetings_links(config, curr_committee_meetings_df=None, duckdb_conn = None):
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Starting to process committee meetings links data")
     committee_meetings_df = get_committee_meetings_data(config)
@@ -126,6 +135,7 @@ def committee_meetings_links(config, curr_committee_meetings_df=None, duckdb_con
     log_bq_load('lgover', OUT_DATASET_NAME, 'committee_meetings_links', ENV, 'drop', duckdb_conn)
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Committee meetings links data processing complete")
 
+@task(retries=0, retry_delay_seconds=10, log_prints=True, cache_policy=NO_CACHE)
 def companions(raw_bills_df, curr_companions_df=None, duckdb_conn = None):
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Starting to process companions data")
     companions_df = get_companions_data(raw_bills_df)
@@ -135,6 +145,7 @@ def companions(raw_bills_df, curr_companions_df=None, duckdb_conn = None):
     log_bq_load('lgover', OUT_DATASET_NAME, 'companions', ENV, 'drop', duckdb_conn)
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Companions data processing complete")
 
+@task(retries=0, retry_delay_seconds=10, log_prints=True, cache_policy=NO_CACHE)
 def complete_bills_list(raw_bills_df, curr_complete_bills_list_df=None, duckdb_conn = None):
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Starting to process complete bills list data")
     complete_bills_list_df = get_complete_bills_list(raw_bills_df)
@@ -145,6 +156,7 @@ def complete_bills_list(raw_bills_df, curr_complete_bills_list_df=None, duckdb_c
     log_bq_load('lgover', OUT_DATASET_NAME, 'complete_bills_list', ENV, 'drop', duckdb_conn)
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Complete bills list data processing complete")
 
+@task(retries=0, retry_delay_seconds=10, log_prints=True, cache_policy=NO_CACHE)
 def links(raw_bills_df, config, curr_links_df=None, duckdb_conn = None):
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Starting to process links data")
     links_df = get_links_data(raw_bills_df,config)
@@ -155,6 +167,7 @@ def links(raw_bills_df, config, curr_links_df=None, duckdb_conn = None):
     log_bq_load('lgover', OUT_DATASET_NAME, 'links', ENV, 'drop', duckdb_conn)
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Links data processing complete")
 
+@task(retries=0, retry_delay_seconds=10, log_prints=True, cache_policy=NO_CACHE)
 def sponsors(raw_bills_df, curr_sponsors_df=None, duckdb_conn = None):
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Starting to process sponsors data")
     sponsors_df = get_sponsors_data(raw_bills_df)
@@ -165,6 +178,7 @@ def sponsors(raw_bills_df, curr_sponsors_df=None, duckdb_conn = None):
     log_bq_load('lgover', OUT_DATASET_NAME, 'sponsors', ENV, 'drop', duckdb_conn)
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Sponsors data processing complete")
 
+@task(retries=0, retry_delay_seconds=10, log_prints=True, cache_policy=NO_CACHE)
 def subjects(raw_bills_df, curr_subjects_df=None, duckdb_conn = None):
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Starting to process subjects data")
     subjects_df = get_subjects_data(raw_bills_df)
@@ -175,7 +189,7 @@ def subjects(raw_bills_df, curr_subjects_df=None, duckdb_conn = None):
     log_bq_load('lgover', OUT_DATASET_NAME, 'subjects', ENV, 'drop', duckdb_conn)
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Subjects data processing complete")
 
-
+@task(retries=0, retry_delay_seconds=10, log_prints=True, cache_policy=NO_CACHE)
 def rss_feeds(config, curr_rss_df=None, duckdb_conn = None):
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Starting to process RSS feeds data")
     rss_df = get_rss_data(config)
@@ -186,6 +200,7 @@ def rss_feeds(config, curr_rss_df=None, duckdb_conn = None):
     log_bq_load('lgover', OUT_DATASET_NAME, 'rss_feeds', ENV, 'drop', duckdb_conn)
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- RSS feeds data processing complete")
 
+@task(retries=0, retry_delay_seconds=10, log_prints=True, cache_policy=NO_CACHE)
 def upcoming_committee_meetings(config, duckdb_conn):
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Starting to process upcoming committee meetings data")
     upcoming_meetings_df = get_upcoming_committee_meetings(config)
@@ -195,6 +210,7 @@ def upcoming_committee_meetings(config, duckdb_conn):
     log_bq_load('lgover', OUT_DATASET_NAME, 'upcoming_committee_meetings', ENV, 'append', duckdb_conn)
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Upcoming committee meetings data processing complete")
 
+@task(retries=0, retry_delay_seconds=10, log_prints=True, cache_policy=NO_CACHE)
 def upcoming_committee_meeting_bills(config, duckdb_conn):
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Starting to process upcoming committee meeting bills data")
     upcoming_meeting_bills_df = get_upcoming_committee_meeting_bills(config)
@@ -204,6 +220,7 @@ def upcoming_committee_meeting_bills(config, duckdb_conn):
     log_bq_load('lgover', OUT_DATASET_NAME, 'upcoming_committee_meeting_bills', ENV, 'append', duckdb_conn)
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Upcoming committee meeting bills data processing complete")
 
+@task(retries=0, retry_delay_seconds=10, log_prints=True, cache_policy=NO_CACHE)
 def versions(raw_bills_df, curr_versions_df=None, duckdb_conn = None):
     logger.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -- Starting to process versions data")
     versions_df = get_versions_data(raw_bills_df)
@@ -222,6 +239,7 @@ def versions(raw_bills_df, curr_versions_df=None, duckdb_conn = None):
 def tx_leg_pipeline():
 
     logger = logging.getLogger(__name__)
+    print('USING ENV: ', ENV)
 
     start_time = datetime.datetime.now()
     with open(CONFIG_PATH, "r", encoding="utf-8") as f:
