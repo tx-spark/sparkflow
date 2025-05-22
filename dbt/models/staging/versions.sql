0c8de24d9d9e12a3908fc3779d3433cd55ab4f2f
lwgover <lucasgover@gmail.com>
2025-05-22 00:39:00 -0400

    removing duplicate versions

--- dbt/models/staging/versions.sql
+++ dbt/models/staging/versions.sql
@@ -28,23 +28,30 @@ most_recent_timestamp as ( -- I'm assuming that there are no bills without versi
     select leg_id,max(last_seen_at) as seen_at_timestamp
     from versions
     group by leg_id
-)
+),
 
 -- remove all the versions that have dropped off
-SELECT
-    bill_id,
-    versions.leg_id,
-    type,
-    text_order,
-    description,
-    html_url,
-    pdf_url,
-    ftp_html_url,
-    ftp_pdf_url,
-    text,
-    first_seen_at,
-    last_seen_at
-FROM versions
-inner join most_recent_timestamp
-    on versions.last_seen_at = most_recent_timestamp.seen_at_timestamp 
-    and versions.leg_id = most_recent_timestamp.leg_id
+
+most_recent_versions as (
+    SELECT -- remove all the versions that have dropped off
+        bill_id,
+        versions.leg_id,
+        type,
+        text_order,
+        description,
+        html_url,
+        pdf_url,
+        ftp_html_url,
+        ftp_pdf_url,
+        text,
+        first_seen_at,
+        last_seen_at,
+        row_number() over (PARTITION BY bill_id, versions.leg_id, type, text_order, description, html_url, pdf_url, ftp_html_url, ftp_pdf_url, first_seen_at, last_seen_at) as rn
+    FROM versions
+    inner join most_recent_timestamp
+        on versions.last_seen_at = most_recent_timestamp.seen_at_timestamp 
+        and versions.leg_id = most_recent_timestamp.leg_id
+)
+
+select * except(rn) from most_recent_versions where rn = 1
+
