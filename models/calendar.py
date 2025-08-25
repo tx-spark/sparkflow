fe3efc132bfd835d10a0cddccdd4f72a28b9888a
Kevin Chen <kevin.chen.weijie@gmail.com>
2025-08-24 20:24:06 -0700

    add subcalendars to calendar

--- models/calendar.py
+++ models/calendar.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from datetime import datetime
 
 from .chamber import Chamber
+from .subcalendar import Subcalendar
 
 
 @dataclass(frozen=True)
@@ -9,4 +10,4 @@ class Calendar:
     chamber: Chamber
     calendar_type: str
     calendar_date: datetime
-    bill_ids: list[str]
+    subcalendars: list[Subcalendar]
